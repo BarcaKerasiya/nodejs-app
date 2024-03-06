cc43c8621ceb1ae9670fc051d4f44723bb6cfb2c
Kerasiya Vishnu <vishnuk@ziguts.com>
2024-03-06 10:24:19 +0000

    changes

--- mongoC.js
+++ mongoC.js
@@ -1,13 +1,18 @@
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
+console.log("process.env.MONGO_PASSWORD", process.env.MONGO_PASSWORD);
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-const connectionString = `mongodb+srv://integrationninjas:${password}@devcluster.xf2gcci.mongodb.net/?retryWrites=true&w=majority`; // clustore url
+// const connectionString = `mongodb+srv://integrationninjas:${password}@devcluster.xf2gcci.mongodb.net/?retryWrites=true&w=majority`; // clustore url
+const connectionString = `mongodb+srv://BlueMarron:${password}@bluemarron.496xb.mongodb.net/my_website1?retryWrites=true&w=majority`;
+
 const client = new MongoClient(connectionString);
 let conn;
 try {
   conn = await client.connect();
-  console.log("connection successful")
-} catch(e) {
+  console.log("connection successful");
+} catch (e) {
   console.error(e);
 }
 let db = conn.db("integration_ninjas");
